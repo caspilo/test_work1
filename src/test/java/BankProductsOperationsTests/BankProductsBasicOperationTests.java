a6173447f0b111b38204fc8a373e0d5f7d8df4d4
caspilo <usov.vanya@bk.ru>
2025-06-16 13:29:30 +0500

    Completed

--- src/test/java/BankProductsOperationsTests/BankProductsBasicOperationTests.java
+++ src/test/java/BankProductsOperationsTests/BankProductsBasicOperationTests.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Execution(ExecutionMode.CONCURRENT)
 public class BankProductsBasicOperationTests {
 
@@ -44,27 +46,27 @@ public class BankProductsBasicOperationTests {
 
     @Test
     void getBalanceBankProductsTest() {
-        Assertions.assertEquals(100, debetCard.getBalance());
-        Assertions.assertEquals(200, debetCurrencyCard.getBalance());
-        Assertions.assertEquals(1000, creditCard.getBalance());
-        Assertions.assertEquals(300, contribution.getBalance());
+        assertEquals(100, debetCard.getBalance());
+        assertEquals(200, debetCurrencyCard.getBalance());
+        assertEquals(1000, creditCard.getBalance());
+        assertEquals(300, contribution.getBalance());
     }
 
     @Test
     void getNameBankProductsTest() {
-        Assertions.assertEquals("DebetCard", debetCard.getName());
-        Assertions.assertEquals("DebetCurrencyCard", debetCurrencyCard.getName());
-        Assertions.assertEquals("CreditCard", creditCard.getName());
-        Assertions.assertEquals("Contribution", contribution.getName());
+        assertEquals("DebetCard", debetCard.getName());
+        assertEquals("DebetCurrencyCard", debetCurrencyCard.getName());
+        assertEquals("CreditCard", creditCard.getName());
+        assertEquals("Contribution", contribution.getName());
 
     }
 
     @Test
     void getCurrencyBankProductsTest() {
-        Assertions.assertEquals("RUB", debetCard.getCurrency());
-        Assertions.assertEquals("USD", debetCurrencyCard.getCurrency());
-        Assertions.assertEquals("RUB", creditCard.getCurrency());
-        Assertions.assertEquals("RUB", contribution.getCurrency());
+        assertEquals("RUB", debetCard.getCurrency());
+        assertEquals("USD", debetCurrencyCard.getCurrency());
+        assertEquals("RUB", creditCard.getCurrency());
+        assertEquals("RUB", contribution.getCurrency());
     }
 
     @Test
@@ -74,10 +76,10 @@ public class BankProductsBasicOperationTests {
         }
         contribution.replenishment(50);
 
-        Assertions.assertEquals(150, debetCard.getBalance());
-        Assertions.assertEquals(250, debetCurrencyCard.getBalance());
-        Assertions.assertEquals(1050, creditCard.getBalance());
-        Assertions.assertEquals(350, contribution.getBalance());
+        assertEquals(150, debetCard.getBalance());
+        assertEquals(250, debetCurrencyCard.getBalance());
+        assertEquals(1050, creditCard.getBalance());
+        assertEquals(350, contribution.getBalance());
     }
 
     @Test
@@ -85,14 +87,14 @@ public class BankProductsBasicOperationTests {
         for (Card card : cardList) {
             card.writeOff(50);
         }
-        Assertions.assertEquals(50, debetCard.getBalance());
-        Assertions.assertEquals(150, debetCurrencyCard.getBalance());
-        Assertions.assertEquals(950, creditCard.getBalance());
+        assertEquals(50, debetCard.getBalance());
+        assertEquals(150, debetCurrencyCard.getBalance());
+        assertEquals(950, creditCard.getBalance());
     }
 
     @Test
     void getDebtForCreditCardTest() {
-        Assertions.assertEquals(400, creditCard.getDebt());
+        assertEquals(400, creditCard.getDebt());
     }
 
     @Test
@@ -100,12 +102,14 @@ public class BankProductsBasicOperationTests {
         contribution.closeContribution();
         Assertions.assertTrue(contribution.isClosed());
     }
+
     @Test
     void writeOffWithExceptionForCardsTest() {
         for (Card card : cardList) {
             Assertions.assertThrows(IllegalArgumentException.class, () -> card.writeOff(10000));
         }
     }
+
     @Test
     void closeAlreadyClosedContributionTest() {
         contribution.closeContribution();
